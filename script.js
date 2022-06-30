d7a13e83f8c16d046e7ed8f0fd93e4fd41549d94
NailaServilha <nailaservilha1212@gmail.com>
2022-06-29 21:04:33 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,37 @@
+const nome              = "Naila"
+const sobrenome         = "Rocha"
+const data_nascimento   = "03/04/2006"
+
+let altura              = 1.66
+let numero_sapato       = 38
+let idade               = 16
+
+let celular           = "(18) 99124 - 0271"
+let cor_favorita        = 'preto'
+
+let qtde_moto           = null
+let qtde_carro          = null
+
+
+//                      <- o valor
+qtde_carro = 1
+
+altura = 1.65
+
+celular = "(18) 99124-0271"
+
+// comparar/validar/verificar igualdade
+
+nome.trim().toUpperCase()== "naila" // false
+
+idade == 16 // true
+
+idade = 17
+
+idade == 17 // true
+
+(true == false) == (true == false)
+
+// igualdade
+
+nome === "naila"
